eff38c8c4eafd5490bf5ece749fb0c9f10410ec4
pc <tiznadoerick3@gmail.com>
2023-05-25 07:47:08 -0600

    Nuevos cambios

---
+++ public/js/components/UsersTable.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <input type="text" v-model="search" placeholder="Buscar usuario">
+    <table>
+      <tr v-for="user in filteredUsers" :key="user.id">
+        <td><img :src="user.profile_picture" alt="User Profile Picture"></td>
+        <td>{{ user.name }}</td>
+        <td><button @click="viewStats(user)">Ver estadísticas</button></td>
+        <td><button @click="chatWith(user)">Chatear</button></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [],
+      search: ''
+    }
+  },
+  computed: {
+    filteredUsers() {
+      return this.users.filter(user => user.name.includes(this.search));
+    }
+  },
+  methods: {
+    viewStats(user) {
+      // Lógica para ver las estadísticas del usuario
+    },
+    chatWith(user) {
+      // Lógica para chatear con el usuario
+    }
+  },
+  mounted() {
+    axios.get('/api/users')
+      .then(response => {
+        this.users = response.data;
+      });
+  }
+}
+</script>
